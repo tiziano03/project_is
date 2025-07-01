aa0291a3fcbd926087cc179a35b057ece2ac31bf
tiziano03 <132991779+tiziano03@users.noreply.github.com>
2025-07-01 14:09:23 +0200

    Aggiunta branch mvc-branch

---
+++ ControllerLibreria.java
@@ -0,0 +1,43 @@
+
+    public class ControllerLibreria {
+        private GestorePersistenza gp;
+        private Libreria lib;
+        private VistaLibreria vista;
+
+
+
+
+        public void salvataggio(String path){}
+
+
+        public void caricamento(String path){}
+
+
+        public void aggiungiLibro(Libro lib){}
+
+
+        public void rimuoviLibro(Libro lib){}
+
+
+        public void modificaLibro(Libro lib){}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
---
+++ VistaLibreria.java
@@ -0,0 +1,9 @@
+public interface VistaLibreria {
+
+
+    public void mostraMessaggio(String messaggio);
+
+
+
+
+}
